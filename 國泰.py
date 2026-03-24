ea5fb340f7c7b0f62a7f60a41aa7d1327bf4c2e8
CA <130337832+peterca1992@users.noreply.github.com>
2026-03-24 18:39:47 +0800

    Add files via upload

---
+++ 國泰.py
@@ -0,0 +1,80 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+#%%
+
+chromeOptions = webdriver.ChromeOptions()
+chromeOptions.add_experimental_option('useAutomationExtension', False)
+
+driver = webdriver.Chrome()
+
+st_list = pd.read_csv("D:\\Python_code\\工作內容\\2026\\0320_參與券商確認\\ST.csv")
+
+
+#%%
+st_list = ["E66", "E82", "E83", "E84", "E87", "E88", "E93", "E94", "EA8", "EDY", "EA9", "EAA", "EAD", "EAE", "EAJ", "EAK", "EAL", "EAW", "EAU", "EAV"]
+
+
+df_output = pd.DataFrame()
+
+for i in range(0, len(st_list)):
+    
+    url = "https://www.cathaysite.com.tw/ETF/detail/E83"
+    url = "https://www.cathaysite.com.tw/ETF/detail/E66"
+    
+    driver.get(url)
+    time.sleep(2)
+    
+    participate_company_click = driver.find_element(By.XPATH, '//*[@id="basic_info"]/app-basic-info/div/div/div[3]/div/div[4]')
+    participate_company_click.click()
+    
+    
+    participate_company = driver.find_element(By.XPATH, '//*[@id="basic_info"]/app-basic-info/div/div/div[3]/div/div[5]/div/div[2]')
+
+    
+    participate_company = participate_company.text.split("\n")
+    
+    participate_company_str = ""
+ 
+    for j in participate_company:
+     
+        if(j.split(" ")[0].find("股份有限公司") != -1):
+            participate_company_str = participate_company_str + j.split(" ")[0] + "、"
+ 
+    participate_company_str = participate_company_str[:-1]
+    
+    st_id = driver.find_element(By.XPATH, '/html/body/app-root/app-etf/app-etf-detail/app-banner/div[1]/div/div[2]/div[1]/div[2]/div[2]/h1')
+    st_id.text
+    
+    df_output_part = pd.DataFrame([st_id.text.split(" ")[0], st_id.text.split(" ")[1], participate_company_str]).T
+    
+    df_output = pd.concat([df_output, df_output_part], axis = 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
